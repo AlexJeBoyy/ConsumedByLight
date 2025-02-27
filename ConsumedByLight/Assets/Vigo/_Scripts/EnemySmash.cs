5ea9adefc38dc5ea73e332296eb7337c80d64966
Vigo-l <v.linders@sintlucasedu.nl>
2025-02-27 16:12:17 +0100

    Polished telekenisis and added enemy velocity check

---
+++ ConsumedByLight/Assets/Vigo/_Scripts/EnemySmash.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class EnemySmash : MonoBehaviour
+{
+    private Rigidbody rb;
+    [SerializeField] private float speedOfObject;
+    [SerializeField] private float destroySpeedThreshold = 10f;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        speedOfObject = rb.velocity.magnitude;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+
+        if (speedOfObject >= destroySpeedThreshold)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
