54a803f1f842b4c168302af355a959d5db19566b
Vigo-l <v.linders@sintlucasedu.nl>
2025-03-27 14:59:41 +0100

    Revert "stamina refined"
    This reverts commit 46c15bbc5c436100026d058e878ce5e5a5e2c6b0.

--- ConsumedByLight/Assets/Vigo/_Scripts/playerGrab.cs
+++ ConsumedByLight/Assets/Vigo/_Scripts/playerGrab.cs
@@ -50,7 +50,7 @@ public class PlayerGrab : MonoBehaviour
     {
         if (grabbedRB)
         {
-
+            usingStamina = true;
             float distanceFromCamera = Vector3.Distance(cam.transform.position, objectHolder.position);
             targetPosistion = cam.transform.position + cam.transform.forward * distanceFromCamera;
 
@@ -87,21 +87,12 @@ public class PlayerGrab : MonoBehaviour
             {
                 chargeTime += Time.deltaTime * chargeSpeed;
                 chargeTime = Mathf.Clamp(chargeTime, 0f, maxChargeTime);
-                usingStamina = false;
             }
         }
     }
 
     private void Update()
     {
-        if (grabbedRB != null)
-        {
-            usingStamina = true;
-        }
-        else
-        {
-            usingStamina = false;
-        }
         staminaGrab.value = currentStamina;
         staminaGrab2.value = currentStamina;
 
@@ -168,7 +159,6 @@ public class PlayerGrab : MonoBehaviour
         if (ctx.performed && grabbedRB != null)
         {
             isCharging = true;
-            usingStamina = false;
         }
         else if (ctx.canceled && grabbedRB != null)
         {
